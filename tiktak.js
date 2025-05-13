6ff2fbbc8a499c5d712428fdbee469d282c4cdff
JoonaHanttu <155952830+JoonaHanttu@users.noreply.github.com>
2025-05-13 11:20:15 +0300

    Add files via upload

---
+++ tiktak.js
@@ -0,0 +1,13 @@
+function playerplace(id) {
+    const boxid = id;
+    if (currentValue != "X"){
+        currentValue = "X"
+        document.getElementById(boxid).innerHTML = currentValue;
+    }
+    
+    else{
+        currentValue = "O"
+        document.getElementById(boxid).innerHTML = currentValue;
+    }
+  }
+var currentValue = "O"
