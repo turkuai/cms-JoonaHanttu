b4f2ad2d1bf6d1cdfba71fa296847f83b14a2218
JoonaHanttu <jh0878@edu.turku.fi>
2025-05-28 12:24:55 +0300

    kovatyö

--- xo/db.php
+++ xo/db.php
@@ -1,8 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "tictactoe";
+foreach (parse_ini_file('.env') as $key => $value) {
+  $_ENV[$key] = $value;
+}
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
